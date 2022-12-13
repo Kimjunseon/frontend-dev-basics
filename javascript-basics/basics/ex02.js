b30a132db7bc211f05ec10dc265784ded22d252c
Kimjunseon <junseon0722@gmail.com>
2022-12-13 09:56:21 +0900

    js 1213 #1

---
+++ javascript-basics/basics/ex02.js
@@ -0,0 +1,16 @@
+/* 
+변수와 자료형(Data Type)
+
+기본타입(Primitive Type)
+- number(실수와 정수를 따로 구분하진 않음)
+- string
+- boolean
+
+*/
+i = 10;
+s = "Hello World";
+b = true;
+
+console.log(i + ": " + typeof(i));
+console.log(s + ": " + typeof(s));
+console.log(b + ": " + typeof(b));
