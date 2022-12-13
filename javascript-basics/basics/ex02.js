06dfb329ba50156f02541bf3e14c2a0071d161a3
Kimjunseon <junseon0722@gmail.com>
2022-12-13 10:13:19 +0900

    js 1213 #1

--- javascript-basics/basics/ex02.js
+++ javascript-basics/basics/ex02.js
@@ -1,14 +1,19 @@
 /* 
 변수와 자료형(Data Type)
 
-기본타입(Primitive Type)
+[기본타입(Primitive Type)]
 - number(실수와 정수를 따로 구분하진 않음)
+- undefined
 - string
 - boolean
 
+[객체]
+object
+function
+
 */
 
-var u;
+var u = undefined; // var u; 동일, 선언 정의가 구분이 없다.
 
 i = 10;
 s = "Hello World";
@@ -17,3 +22,5 @@ b = true;
 console.log(i + ": " + typeof(i));
 console.log(s + ": " + typeof(s));
 console.log(b + ": " + typeof(b));
+
+console.log(s.toUpperCase()); // 유사 객체?
